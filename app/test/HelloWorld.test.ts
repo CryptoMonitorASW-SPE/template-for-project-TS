1196f2b968a13513891f823736a10e5a4f3f865a
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-23 18:38:55 +0100

    ci: change ci to use default test reporter

--- app/test/HelloWorld.test.ts
+++ app/test/HelloWorld.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 
 describe('HelloWorld Test Suite', () => {
   it('should return correct greeting', () => {
-    const greeting = 'Hello World?';
+    const greeting = 'Hello World!';
     expect(greeting).to.equal('Hello World!');
   });
 });
